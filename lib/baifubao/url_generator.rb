eb405ff5be6d2658653af29766325ff2601d1138
yunwei <stillwyw@gmail.com>
2016-07-07 16:27:54 +0800

    set default and must variables.

--- lib/baifubao/url_generator.rb
+++ lib/baifubao/url_generator.rb
@@ -4,9 +4,12 @@ module Baifubao
     def initialize(url, params, options={})
       @url = url
       @params = params
-      @params[:sp_no] = params[:sp_no] || Baifubao::Config.sp_no
-      @params[:key] = params[:key] || Baifubao::Config.key
+      @params[:sp_no] ||= Baifubao::Config.sp_no
+      @params[:key] ||= Baifubao::Config.key
       @params[:sign] = Baifubao::Signature.sign(@params)
+      @params[:version] ||= 2
+      @params[:input_charset] ||= 1
+      @params[:sign_method] ||= 1
       @url += @params.map{|m| m.join("=")}.join("&")
     end
     
